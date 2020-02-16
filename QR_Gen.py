d6321f97020338c211dbc624efef0adba98d55a7
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-02-16 17:34:37 +0530

    First Commit

---
+++ QR_Gen.py
@@ -0,0 +1,20 @@
+import Tkinter as tk
+import pyqrcode
+win = tk.Tk()
+win.title("QE Code Generator")
+
+def generator():
+    url = entry.get()
+    qr = pyqrcode.create(url)
+    save = qr.svg("myqrcode.svg",scale=10)
+
+label1 = tk.Label(win,text="Enter URL ")
+label1.grid(row=0,column=0)
+
+entry = tk.Entry(win)
+entry.grid(row=1,column=0)
+
+button = tk.Button(win,text="Generate",command=generator)
+button.grid(row = 3,column = 0)
+
+win.mainloop()
